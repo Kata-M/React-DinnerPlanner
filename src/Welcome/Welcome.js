4711274959f11a113abd7f99827cc86f0c290fb0
Kata-M <martikai@kth.se>
2019-02-21 14:59:05 +0100

    fix welcome screen layout

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -7,12 +7,12 @@ class Welcome extends Component {
     return (
       <div className="Welcome">
 
-        <h1>Welcome to the dinner planner React Startup code!</h1>
+        <h1 className="WelcomeTextH">Welcome to the dinner planner React project!</h1>
         
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-        
+        <p className="WelcomeTextP">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+         
         <Link to="/search">
-            <button>Start planning</button>
+            <button className="StartPlanningBtn"> Start planning</button>
         </Link>
       </div>
     );
